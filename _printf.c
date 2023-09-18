e13e9dbbd7991e55e63d3c60117175d81e51d69d
Walaafadol <walaafadul7@gmail.com>
2023-09-18 05:45:19 -0700

    task

--- _printf.c
+++ _printf.c
@@ -9,11 +9,11 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int char_count = 0, i = 0;
-va_start(args, format);
 if (!format || (format[0] == '%' && !format[1]))
 return (-1);
 if (format[0] == '%' && format[1] == ' ' && !format[2])
 return (-1);
+va_start(args, format);
 for ( ; format[i] != '\0'; i++)
 {
 if (format[i] != '%')
