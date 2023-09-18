ae05ab89a0c0f2fc8de2f6cd2069ba0f0afc92ea
Walaafadol <walaafadul7@gmail.com>
2023-09-18 06:12:13 -0700

    jjj

--- _printf.c
+++ _printf.c
@@ -8,13 +8,11 @@
 int _printf(const char *format, ...)
 {
 va_list args;
-int char_count = 0, i = 0;
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
+unsigned int leng, i, char_count = 0;
+if (!format || (format[0] == '%' &&format[1] == '\0'))
 return (-1);
 va_start(args, format);
-for ( ; format[i] != '\0'; i++)
+for (i = 0; format[i] != '\0'; i++)
 {
 if (format[i] != '%')
 {
@@ -27,7 +25,7 @@ i++;
 }
 else if (format[i + 1] == 's')
 {
-int leng = put_s(va_arg(args, char *));
+leng = put_s(va_arg(args, char *));
 i++;
 char_count += (leng - 1);
 }
