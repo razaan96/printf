f099117878ffdb462572c2048a5d2753defe3d02
Razan <razaan.alaa@hotmail.com>
2023-09-18 20:29:46 -0700

    lets hope for the best

--- _printf.c
+++ _printf.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
 *_printf - program
-*Return:0
+*Return:the number of characters printed
 *@format:parameter
 *@...:argument
 */
 int _printf(const char *format, ...)
 {
 va_list args;
-int i, char_count = 0;
+unsigned int i, char_count = 0;
 if (!format || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 if (format[0] == '%' && format[1] == ' ' && !format[2])
@@ -20,18 +20,18 @@ if (format[i] != '%')
 {
 putchr(format[i]);
 }
-else if (format[i + 1] == 'c')
+else if (format[i] == '%' && format[i + 1] == 'c')
 {
 putchr(va_arg(args, int));
 i++;
 }
-else if (format[i + 1] == 's')
+else if (format[i] == '%' && format[i + 1] == 's')
 {
 int leng = put_s(va_arg(args, char *));
 i++;
 char_count += (leng - 1);
 }
-else if (format[i + 1] == '%')
+else if (format[i] == '%' && format[i + 1] == '%')
 {
 putchr('%');
 i++;
