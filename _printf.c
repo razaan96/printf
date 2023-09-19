6025b9df0027b276b8441b942056988575aaf036
Razan <razaan.alaa@hotmail.com>
2023-09-18 21:56:48 -0700

    I am not going anywhere. You can print that wherever you want to. I am here and I am a Spur for life

--- _printf.c
+++ _printf.c
@@ -8,10 +8,8 @@
 int _printf(const char *format, ...)
 {
 va_list args;
-unsigned int i, char_count = 0;
+unsigned int char_count, pfret = 0, i;
 if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
 return (-1);
 va_start(args, format);
 for (i = 0; format && format[i] != '\0'; i++)
@@ -36,7 +34,14 @@ else if (format[i] == '%' && format[i + 1] == '%')
 putchr('%');
 i++;
 }
-char_count += 1;
+else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+{
+char_count += putchr(va_arg(args, int));
+i++;
+pfret += (char_count - 1);
+putchr('%');
+}
+pfret += 1;
 }
 va_end(args);
 return (char_count);
